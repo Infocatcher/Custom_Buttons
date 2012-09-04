be22c0ef86e4518a230caf0443863576709b91d4
Infocatcher <infocatcher.dev.null@gmail.com>
2012-09-04 22:25:51 +0400

    Add link to forum topic

--- Undo_Close_Tabs/undoCloseTabs.js
+++ Undo_Close_Tabs/undoCloseTabs.js
@@ -1,4 +1,5 @@
 ﻿// http://infocatcher.ucoz.net/js/cb/undoCloseTabs.js
+// http://forum.mozilla-russia.org/viewtopic.php?pid=580309
 // https://github.com/Infocatcher/Custom_Buttons/tree/master/Undo_Close_Tabs
 
 // Undo Close Tabs button for Custom Buttons
