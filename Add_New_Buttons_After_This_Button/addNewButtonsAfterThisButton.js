774489769bb632a7b0bfce39f287ac0e4a825b01
Infocatcher <infocatcher.dev.null@gmail.com>
2012-09-23 22:45:37 +0400

    Add New Buttons After This Button 0.1.0 - 2012-07-01

---
+++ Add_New_Buttons_After_This_Button/addNewButtonsAfterThisButton.js
@@ -0,0 +1,40 @@
+// https://github.com/Infocatcher/Custom_Buttons/tree/master/Add_New_Buttons_After_This_Button
+
+// Add New Buttons After This Button button for Custom Buttons
+// (code for "initialization" section)
+// Note: button are disabled by default, click to enable
+
+// (c) Infocatcher 2012
+// version 0.1.0 - 2012-07-01
+
+var cbs = Components.classes["@xsms.nm.ru/custombuttons/cbservice;1"]
+	.getService(Components.interfaces.cbICustomButtonsService);
+var windowId = cbs.getWindowId(document.documentURI);
+var notificationPrefix = cbs.getNotificationPrefix(windowId);
+
+this.onclick = function(e) {
+	if(e.button == 0) {
+		this.checked = !this.checked;
+		document.persist(this.id, "checked");
+	}
+};
+
+var observer = {
+	button: this,
+	observe: function(button, topic, data) {
+		if(topic != notificationPrefix + "installButton")
+			return;
+		if(!this.button.checked)
+			return;
+		var toolbar = this.button.parentNode;
+		toolbar.insertBefore(button, this.button.nextSibling);
+		custombuttons.persistCurrentSets(toolbar.id, this.button.id, button.id || button.getAttribute("id"));
+	}
+};
+var os = Components.classes["@mozilla.org/observer-service;1"]
+	.getService (Components.interfaces.nsIObserverService);
+os.addObserver(observer, notificationPrefix + "installButton", false);
+
+this.onDestroy = function() {
+	os.removeObserver(observer, notificationPrefix + "installButton");
+};
