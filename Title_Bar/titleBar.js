918c46431909a4ed47e219a84c521213a867ac26
Infocatcher <infocatcher.dev.null@gmail.com>
2012-09-25 16:44:29 +0400

    Center label

--- Title_Bar/titleBar.js
+++ Title_Bar/titleBar.js
@@ -56,6 +56,10 @@ var cssStr = ('\
 			min-width: ' + titleWidthMin + ' !important;\n\
 			max-width: ' + titleWidthMax + ' !important;\n\
 			-moz-box-flex: 1 !important;\n\
+			padding-top: 1px !important;\n\
+			padding-bottom: 1px !important;\n\
+			-moz-box-pack: center !important;\n\
+			-moz-box-orient: horizontal !important;\n\
 			background: transparent !important;\n\
 			-moz-box-shadow: none !important;\n\
 			-moz-appearance: none !important;\n\
