d0f65d21b5b550d230474e409d0456ebe5e4d8cf
Infocatcher <infocatcher.dev.null@gmail.com>
2012-10-12 13:43:35 +0400

    Pin Tabs v. 0.1.1 - 2010-08-08

---
+++ Pin_Tabs/pinTabs.js
@@ -0,0 +1,78 @@
+// Pin Tabs for Custom Buttons
+// version 0.1.1 - 2010-08-08
+// (code for "initialization" section)
+// (c) Infocatcher 2010
+
+//== Settings begin
+this.startupAutoPin = false;
+// See https://developer.mozilla.org/en/XUL_Tutorial/Keyboard_Shortcuts#Key_element
+addKey(this, "toggleTabPinned",  "control q");
+addKey(this, "toggleTabsPinned", "control shift q");
+addKey(this, "toggleAutoPin",    "control alt q");
+//== Settings end
+
+this.toggleTabPinned = function(tab) {
+	tab = tab || gBrowser.selectedTab;
+	gBrowser[tab.pinned ? "unpinTab" : "pinTab"](tab);
+};
+this.toggleTabsPinned = function(pin) {
+	var tabs = Array.slice(gBrowser.tabs);
+	pin = arguments.length
+		? pin
+		: tabs.some(function(tab) { return !tab.pinned; });
+	if(!pin)
+		tabs = tabs.reverse();
+	var action = pin ? "pinTab" : "unpinTab";
+	tabs.forEach(function(tab) {
+		gBrowser[action](tab);
+	});
+};
+this.evtHahdler = {
+	button: this,
+	handleEvent: function(e) {
+		gBrowser.pinTab(e.target);
+	}
+};
+this._autoPin = false;
+this.toggleAutoPin = function() {
+	this._autoPin = !this._autoPin;
+	var tc = gBrowser.tabContainer;
+	tc[this._autoPin ? "addEventListener" : "removeEventListener"]("TabOpen", this.evtHahdler, false);
+	this.checked = this._autoPin;
+};
+this.onDestroy = function() {
+	this._autoPin && this.toggleAutoPin();
+};
+this.onclick = function(e) {
+	if(e.button == 0)
+		this.toggleTabsPinned();
+	else if(e.button == 1)
+		this.toggleAutoPin();
+};
+if(this.startupAutoPin) {
+	this.toggleTabsPinned(true);
+	this.toggleAutoPin();
+}
+
+function addKey(button, cmd, keyStr) {
+	if(!addKey.hasOwnProperty("keyset")) {
+		let kId = "custombuttons-keyset";
+		let keyset = document.getElementById(kId);
+		if(!keyset) {
+			keyset = document.createElement("keyset");
+			keyset.id = kId;
+			document.documentElement.appendChild(keyset);
+		}
+		addKey.keyset = keyset;
+	}
+	var keyElt = document.createElement("key");
+	keyElt.id = "custombuttons-key-" + cmd;
+	keyElt.__button = button;
+	keyElt.setAttribute("oncommand", "this.__button." + cmd + "();");
+	var tokens = keyStr.split(" ");
+	var key = tokens.pop() || " ";
+	var modifiers = tokens.join(",");
+	keyElt.setAttribute(key.indexOf("VK_") == 0 ? "keycode" : "key", key);
+	keyElt.setAttribute("modifiers", modifiers);
+	addKey.keyset.appendChild(keyElt);
+}
