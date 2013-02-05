851161276f57d29a86bba3a6a0fc1aff9818cf6c
Infocatcher <infocatcher.dev.null@gmail.com>
2013-02-05 23:39:33 +0400

    Check for STATUS_LOAD_COMPLETE, v. 0.2.0pre - 2013-02-05 + debug logs

--- Reload_Broken_Images/reloadBrokenImages.js
+++ Reload_Broken_Images/reloadBrokenImages.js
@@ -1,10 +1,24 @@
 // Based on code from chrome://browser/content/nsContextMenu.js (Firefox 15.0a1)
+Components.utils.import("resource://gre/modules/Services.jsm");
 function reloadImage(img) {
 	if(!(img instanceof Components.interfaces.nsIImageLoadingContent) || !img.currentURI)
 		return;
-	urlSecurityCheck(img.currentURI.spec,
-	                 gBrowser.contentPrincipal,
-	                 Components.interfaces.nsIScriptSecurityManager.DISALLOW_SCRIPT);
+	var request = img.getRequest(Components.interfaces.nsIImageLoadingContent.CURRENT_REQUEST);
+	if(request && (request.imageStatus & request.STATUS_LOAD_COMPLETE))
+		return;
+	urlSecurityCheck(
+		img.currentURI.spec,
+		img.ownerDocument.nodePrincipal,
+		Components.interfaces.nsIScriptSecurityManager.DISALLOW_SCRIPT
+	);
+	Services.console.logStringMessage("reloadImage(): " + img.currentURI.spec);
+	function check(e) {
+		img.removeEventListener("load", check, true);
+		img.removeEventListener("error", check, true);
+		Services.console.logStringMessage("reloadImage(): " + img.currentURI.spec + " => " + e.type);
+	}
+	img.addEventListener("load", check, true);
+	img.addEventListener("error", check, true);
 	img.forceReload();
 }
 function parseWin(win) {
