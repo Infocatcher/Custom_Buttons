d2b25d987e3a89f1d516076a65ca87e3c8e87999
Infocatcher <infocatcher.dev.null@gmail.com>
2013-02-05 23:40:56 +0400

    Try force reload + remove image from cache, v. 0.2.0pre2 - 2013-02-05

--- Reload_Broken_Images/reloadBrokenImages.js
+++ Reload_Broken_Images/reloadBrokenImages.js
@@ -6,16 +6,45 @@ function reloadImage(img) {
 	var request = img.getRequest(Components.interfaces.nsIImageLoadingContent.CURRENT_REQUEST);
 	if(request && (request.imageStatus & request.STATUS_LOAD_COMPLETE))
 		return;
+	var uri = img.currentURI;
+	var src = uri.spec;
 	urlSecurityCheck(
-		img.currentURI.spec,
+		src,
 		img.ownerDocument.nodePrincipal,
 		Components.interfaces.nsIScriptSecurityManager.DISALLOW_SCRIPT
 	);
-	Services.console.logStringMessage("reloadImage(): " + img.currentURI.spec);
+	Services.console.logStringMessage("reloadImage(): " + src);
+	var errors = 0;
 	function check(e) {
-		img.removeEventListener("load", check, true);
-		img.removeEventListener("error", check, true);
-		Services.console.logStringMessage("reloadImage(): " + img.currentURI.spec + " => " + e.type);
+		if(e.type == "error")
+			++errors;
+		Services.console.logStringMessage("reloadImage(): " + src + " => " + e.type + (errors ? "#" + errors : ""));
+		if(errors && errors < 4) {
+			try {
+				var tools = Components.classes["@mozilla.org/image/tools;1"]
+					.getService(Components.interfaces.imgITools);
+				var cache = "getImgCacheForDocument" in tools // Gecko 18
+					? tools.getImgCacheForDocument(img.ownerDocument)
+					: Components.classes["@mozilla.org/image/cache;1"]
+						.getService(Components.interfaces.imgICache);
+				if(cache.findEntryProperties(uri)) {
+					cache.removeEntry(uri);
+					Services.console.logStringMessage("reloadImage(): " + src + " => remove this URI from cache");
+				}
+			}
+			catch(e) {
+				Services.console.logStringMessage("reloadImage(): " + src + " => cache.removeEntry() failed");
+				Components.utils.reportError(e);
+			}
+			img.src = "about:blank";
+			setTimeout(function() {
+				img.src = src;
+			}, 0);
+		}
+		else {
+			img.removeEventListener("load", check, true);
+			img.removeEventListener("error", check, true);
+		}
 	}
 	img.addEventListener("load", check, true);
 	img.addEventListener("error", check, true);
