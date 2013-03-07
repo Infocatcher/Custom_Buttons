92acf4dd1e8b9f016e7be4608f0285c2b729d35a
Infocatcher <infocatcher.dev.null@gmail.com>
2013-03-07 15:04:09 +0400

    Convert to menu with many extensions, v. 0.3.0pre - 2013-03-07

--- Extensions_Installer/extensionsInstaller.js
+++ Extensions_Installer/extensionsInstaller.js
@@ -2,105 +2,228 @@
 
 // Extensions Installer button for Custom Buttons
 // (code for "code" section)
-// For developers, useful to test restartless extensions
+// For developers, useful to test restartless extensions.
+// Place code to "initialization" section to create a "normal" menu.
 
 // (c) Infocatcher 2013
-// version 0.2.3.1 - 2013-01-29
+// version 0.3.0pre - 2013-03-07
 
-var dir = "d:\\my_extension";
-var xpi = dir + "\\my_extension-latest.xpi";
-var makeExe = "%COMMANDER_PATH%\\utils\\Delayed_Start\\ds.exe";
-var makeArgs = [
+var extensions = {
+	"extensionsId1@example": {
+		name: "Extension 1",
+		dir: "d:\\extensions\\my_extension",
+		xpi: "%d\\my_extension-latest.xpi"
+	},
+	"extensionsId2@example": {
+		name: "Extension 2",
+		dir: "d:\\extensions\\my_extension_2",
+		xpi: "%d\\my_extension2-latest.xpi",
+		makeExe: "d:\\someMaker.exe",
+		makeArgs: ["some", "args", "here"]
+	}
+};
+var _makeExe = "%COMMANDER_PATH%\\utils\\Delayed_Start\\ds.exe";
+var _makeArgs = [
 	"-w",
-	dir + "\\make.bat",
+	"%d\\make.bat",
 	"nodelay"
 ];
-var make = typeof event == "object" && event instanceof Event
-	? !event.ctrlKey && !event.shiftKey && !event.altKey && !event.metaKey
-	: true;
-
-var lockKey = "__extensionsInstaller_" + xpi;
-if(lockKey in window)
-	return;
-window[lockKey] = true;
-function unlock() {
-	setTimeout(function() {
-		delete window[lockKey];
-	}, 300);
-}
-
-var isCb = this instanceof XULElement;
-Components.utils.import("resource://gre/modules/Services.jsm");
-if(isCb) {
-	var btn = this;
-	var imgConnecting = "chrome://browser/skin/tabbrowser/connecting.png";
-	var imgLoading = "chrome://browser/skin/tabbrowser/loading.png";
-	if(Services.appinfo.name == "SeaMonkey")
-		imgConnecting = imgLoading = "chrome://communicator/skin/icons/loading.gif";
-	var origImg = btn.image;
-	btn.image = imgConnecting;
-	var restore = function() {
-		btn.image = origImg;
-		unlock();
-	};
-}
-else {
-	var restore = unlock;
-	notify("Started…", "Started installation…");
-}
-
-var xpiFile = file(xpi);
-if(!xpiFile.exists()) {
-	restore();
-	notify("Error", "File not found:\n" + xpi);
-	return;
-}
-xpi = Services.io.newFileURI(xpiFile).spec;
-
-if(make) try {
-	var process = Components.classes["@mozilla.org/process/util;1"]
-		.createInstance(Components.interfaces.nsIProcess);
-	process.init(file(expandVariables(makeExe)));
-	process.runw(true, makeArgs.map(expandVariables), makeArgs.length);
-	setTimeout(window.focus, 0); // Strange things happens...
-}
-catch(e) {
-	restore();
-	notify("Error", "Can't make *.xpi!\n" + e);
-	Components.utils.reportError(
-		"[Extensions Installer] Can't make *.xpi!\nCommand line:\n"
-		+ expandVariables(makeExe) + "\n" + makeArgs.map(expandVariables).join("\n")
-	);
-	Components.utils.reportError(e);
-	return;
-}
 
 Components.utils.import("resource://gre/modules/AddonManager.jsm");
-AddonManager.getInstallForURL(
-	xpi,
-	function(install) {
-		if(isCb)
-			btn.image = imgLoading;
-		install.addListener({
-			onInstallEnded: function(install, addon) {
-				install.removeListener(this);
-				restore();
-				notify("Ok!", "Successfully installed:\n" + xpi.match(/[^\\\/]*$/)[0]);
-				if(addon.pendingOperations)
-					Application.restart();
+
+var mp = document.createElement("menupopup");
+mp.setAttribute("oncommand", "this.installExtension(event);");
+
+for(var uid in extensions) if(extensions.hasOwnProperty(uid)) {
+	var ext = extensions[uid];
+	var mi = document.createElement("menuitem");
+	mi.className = "menuitem-iconic";
+	mi.setAttribute("cb_uid", uid);
+	mi.setAttribute("label", ext.name);
+	mi.setAttribute("tooltiptext", ext.dir);
+	setIcon(mi, uid);
+	mp.appendChild(mi);
+}
+function setIcon(mi, uid) {
+	AddonManager.getAddonByID(uid, function(addon) {
+		var icon = addon.iconURL || addon.icon64URL
+			|| "chrome://mozapps/skin/extensions/extensionGeneric-16.png";
+		mi.setAttribute("image", icon);
+	});
+}
+
+mp.installExtension = function(e) {
+	var mi = e.target;
+	var uid = mi.getAttribute("cb_uid");
+	if(!uid || !extensions.hasOwnProperty(uid))
+		return;
+	var ext = extensions[uid];
+
+	var make = !event.ctrlKey && !event.shiftKey && !event.altKey && !event.metaKey;
+
+	var dir = ext.dir;
+	function expandDir(s) {
+		return s.replace(/^%d/, dir);
+	}
+	var xpi = expandDir(ext.xpi);
+	var makeExe = ext.makeExe || _makeExe;
+	var makeArgs = ext.makeArgs || _makeArgs;
+
+	var lockKey = "__extensionsInstaller_" + xpi;
+	if(lockKey in window)
+		return;
+	window[lockKey] = true;
+	mi.disabled = true;
+	function unlock() {
+		setTimeout(function() {
+			mi.disabled = false;
+			delete window[lockKey];
+		}, 300);
+	}
+
+	Components.utils.import("resource://gre/modules/Services.jsm");
+	if(isCb) {
+		++btn._extInstallerInProgress;
+		var imgConnecting = "chrome://browser/skin/tabbrowser/connecting.png";
+		var imgLoading = "chrome://browser/skin/tabbrowser/loading.png";
+		if(Services.appinfo.name == "SeaMonkey")
+			imgConnecting = imgLoading = "chrome://communicator/skin/icons/loading.gif";
+		var origImg = btn.image;
+		btn.image = imgConnecting;
+		var restore = function() {
+			if(!--btn._extInstallerInProgress)
+				btn.image = origImg;
+			unlock();
+		};
+	}
+	else {
+		var restore = unlock;
+		notify("Started…", "Started installation…");
+	}
+
+	var xpiFile = file(xpi);
+	if(!xpiFile.exists()) {
+		restore();
+		notify("Error", "File not found:\n" + xpi);
+		return;
+	}
+	xpi = Services.io.newFileURI(xpiFile).spec;
+
+	if(make) try {
+		var process = Components.classes["@mozilla.org/process/util;1"]
+			.createInstance(Components.interfaces.nsIProcess);
+		process.init(file(expandVariables(expandDir(makeExe))));
+		process.runw(true, makeArgs.map(expandDir).map(expandVariables), makeArgs.length);
+		setTimeout(window.focus, 0); // Strange things happens...
+	}
+	catch(e) {
+		restore();
+		notify("Error", "Can't make *.xpi!\n" + e);
+		Components.utils.reportError(
+			"[Extensions Installer] Can't make *.xpi!\nCommand line:\n"
+			+ expandVariables(makeExe) + "\n" + makeArgs.map(expandVariables).join("\n")
+		);
+		Components.utils.reportError(e);
+		return;
+	}
+
+	AddonManager.getInstallForURL(
+		xpi,
+		function(install) {
+			if(isCb)
+				btn.image = imgLoading;
+			install.addListener({
+				onInstallEnded: function(install, addon) {
+					install.removeListener(this);
+					restore();
+					notify("Ok!", "Successfully installed:\n" + xpi.match(/[^\\\/]*$/)[0]);
+					if(addon.pendingOperations)
+						Application.restart();
+				},
+				onInstallFailed: function(install) {
+					install.removeListener(this);
+					var err = "Installation failed\n" + xpi;
+					Components.utils.reportError(err);
+					restore();
+					notify("Error", err);
+				}
+			});
+			install.install();
+		},
+		"application/x-xpinstall"
+	);
+};
+
+var isCb = this instanceof XULElement; // Custom Buttons
+var isCbInit = isCb
+	&& typeof event == "object"
+	&& !("type" in event)
+	&& typeof _phase == "string" && _phase == "init";
+if(isCb) {
+	var btn = this;
+	btn._extInstallerInProgress = 0;
+}
+if(isCbInit) {
+	this.type = "menu";
+	this.orient = "horizontal";
+	this.appendChild(mp);
+
+	this.onmouseover = function(e) {
+		if(e.target != this)
+			return;
+		Array.some(
+			this.parentNode.getElementsByTagName("*"),
+			function(node) {
+				if(
+					node != this
+					&& node.namespaceURI == xulns
+					&& node.boxObject
+					&& node.boxObject instanceof Components.interfaces.nsIMenuBoxObject
+					&& node.open
+				) {
+					node.open = false;
+					this.open = true;
+					return true;
+				}
+				return false;
 			},
-			onInstallFailed: function(install) {
-				install.removeListener(this);
-				var err = "Installation failed\n" + xpi;
-				Components.utils.reportError(err);
-				restore();
-				notify("Error", err);
-			}
-		});
-		install.install();
-	},
-	"application/x-xpinstall"
-);
+			this
+		);
+	};
+}
+else { // Mouse gestures or something other...
+	let e;
+	if(typeof event == "object" && event instanceof Event && "screenX" in event) // FireGestures
+		e = event;
+	else if(this == window && "mgGestureState" in window && "endEvent" in mgGestureState) // Mouse Gestures Redox
+		e = mgGestureState.endEvent;
+	else {
+		let anchor = this instanceof XULElement && this
+			|| window.gBrowser && gBrowser.selectedBrowser
+			|| document.documentElement;
+		if("boxObject" in anchor) {
+			let bo = anchor.boxObject;
+			e = {
+				screenX: bo.screenX,
+				screenY: bo.screenY
+			};
+			if(this instanceof XULElement)
+				e.screenY += bo.height;
+		}
+	}
+	if(!e || !("screenX" in e))
+		throw new Error("[Extensions Installer]: Can't get event object");
+	document.documentElement.appendChild(mp);
+	mp.addEventListener("popuphidden", function destroy(e) {
+		mp.removeEventListener(e.type, destroy, false);
+		setTimeout(function() {
+			//mp.destroyMenu();
+			mp.parentNode.removeChild(mp);
+		}, 0);
+	}, false);
+	mp.openPopupAtScreen(e.screenX, e.screenY);
+}
+
 function notify(title, text) {
 	Components.classes["@mozilla.org/alerts-service;1"]
 		.getService(Components.interfaces.nsIAlertsService)
