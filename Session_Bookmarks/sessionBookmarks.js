e80ed3ec8b62b7abc7b19732a206d13f40a6d7ba
Infocatcher <infocatcher.dev.null@gmail.com>
2013-04-01 00:36:58 +0400

    Bump version to 0.2.1pre - 2013-04-01

--- Session_Bookmarks/sessionBookmarks.js
+++ Session_Bookmarks/sessionBookmarks.js
@@ -6,7 +6,7 @@
 // (code for "initialization" section)
 
 // (c) Infocatcher 2011-2013
-// version 0.2.0 - 2013-01-26
+// version 0.2.1pre - 2013-04-01
 
 // Usage:
 //   Use middle-click or left+click with any modifier to add current tab
