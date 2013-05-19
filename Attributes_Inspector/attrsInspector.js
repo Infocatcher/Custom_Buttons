66744e6ed08dfd00c3f88f9f7d8bc9d1f553a365
Infocatcher <infocatcher.dev.null@gmail.com>
2013-05-19 18:15:59 +0400

    Bump version to 0.6.1 - 2013-05-19

--- Attributes_Inspector/attrsInspector.js
+++ Attributes_Inspector/attrsInspector.js
@@ -3,7 +3,7 @@
 // https://github.com/Infocatcher/Custom_Buttons/tree/master/Attributes_Inspector
 
 // (c) Infocatcher 2010-2013
-// version 0.6.1pre3 - 2013-03-31
+// version 0.6.1 - 2013-05-19
 
 //===================
 // Attributes Inspector button for Custom Buttons
