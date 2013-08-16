3fdf9ab2387663108abfd8de39036498907c61bc
Infocatcher <infocatcher.dev.null@gmail.com>
2013-08-16 20:13:14 +0400

    Quick Filter by Recipient v. 0.1.0 - 2013-08-16

---
+++ Quick_Filter_by_Recipient/README.md
@@ -0,0 +1,3 @@
+**Quick Filter by Recipient** button for [Custom Buttons](https://addons.mozilla.org/addon/custom-buttons/)
+<br>(for <a href="http://getthunderbird.com/">Thunderbird</a>)
+<br>Uses built-in quick filter to filter all messages from any recipient of selected message
---
+++ Quick_Filter_by_Recipient/tbQuickFilterByRecipient.js
@@ -0,0 +1,78 @@
+﻿// https://github.com/Infocatcher/Custom_Buttons/tree/master/Quick_Filter_by_Recipient
+
+// Quick Filter by Recipient button for Custom Buttons
+// (code for "code" section)
+
+// (c) Infocatcher 2013
+// version 0.1.0 - 2013-08-16
+
+function _localize(s, key) {
+	var strings = {
+		"Filter by recipient": {
+			ru: "Фильтр по получателю"
+		},
+		"Select email address to filter:": {
+			ru: "Выберите email-адрес для фильтрации:"
+		}
+	};
+	var locale = Application.prefs.getValue("general.useragent.locale", "en").match(/^[a-z]*/)[0];
+	_localize = !locale || locale == "en"
+		? function(s) {
+			return s;
+		}
+		: function(s) {
+			return strings[s] && strings[s][locale] || s;
+		};
+	return _localize.apply(this, arguments);
+}
+
+var selectedMessages = gFolderDisplay.selectedMessages;
+if(selectedMessages && selectedMessages.length) {
+	var selectedMessage = selectedMessages[0];
+	var recipients = extractAddress(selectedMessage.recipients).split(", ");
+	var cc = extractAddress(selectedMessage.ccList).split(", ");
+	var bcc = extractAddress(selectedMessage.bccList).split(", ");
+	var allRecipients = recipients.concat(cc, bcc).filter(function(eml, i, arr) {
+		return eml && arr.indexOf(eml) == i;
+	});
+
+	var recipient = allRecipients[0];
+	if(allRecipients.length > 1) {
+		var recipientOut = { value: 0 };
+		var ok = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+			.getService(Components.interfaces.nsIPromptService)
+			.select(
+				window,
+				_localize("Filter by recipient"),
+				_localize("Select email address to filter:"),
+				allRecipients.length,
+				allRecipients,
+				recipientOut
+			);
+		if(!ok)
+			return;
+		recipient = allRecipients[recipientOut.value];
+	}
+
+	setFilterOptions({
+		sender:     false,
+		recipients: true,
+		subject:    false,
+		body:       false
+	});
+	var filterField = document.getElementById("qfb-qs-textbox");
+	filterField.value = recipient;
+	filterField.doCommand();
+}
+function extractAddress(header) {
+	return Components.classes["@mozilla.org/messenger/headerparser;1"]
+		.getService(Components.interfaces.nsIMsgHeaderParser)
+		.extractHeaderAddressMailboxes(header);
+}
+function setFilterOptions(opts) {
+	for(var type in opts) if(opts.hasOwnProperty(type)) {
+		var btn = document.getElementById("qfb-qs-" + type);
+		if(btn.checked != opts[type])
+			btn.click();
+	}
+}
