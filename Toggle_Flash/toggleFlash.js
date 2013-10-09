07263425d1f70f3460b2bf69cad05411974ed7f9
Infocatcher <infocatcher.dev.null@gmail.com>
2013-10-09 13:29:17 +0400

    Add support for "ask to activate" plugins (WIP for #19)

--- Toggle_Flash/toggleFlash.js
+++ Toggle_Flash/toggleFlash.js
@@ -10,28 +10,71 @@
 var options = {
 	pluginName: "Shockwave Flash", // Or name of any other plugin
 	searchInTypes: ["plugin"], // Use "extension" to toggle restartless extensions
-	checkedStyle: false,
-	disabledGrayscale: true,
-	disabledOpacity: 0.65 // 0..1, use 0 to disable
+	// Button styles, possible values:
+	// checked:       [true|false]
+	// style:         "any valid CSS"
+	// iconStyle:     "any valid CSS"
+	// iconGrayscale: [true|false]
+	// iconOpacity:   0..1
+	styleEnabled: {
+		checked: false,
+		style: "",
+		iconStyle: "",
+		iconGrayscale: false,
+		iconOpacity: 1,
+	},
+	styleClickToPlay: {
+		checked: false,
+		style: "",
+		iconStyle: "",
+		iconGrayscale: false,
+		iconOpacity: 0.65
+	},
+	styleDisabled: {
+		checked: false,
+		style: "",
+		iconStyle: "",
+		iconGrayscale: true,
+		iconOpacity: 0.65
+	}
 };
 
-var addonId;
-this._pluginEnabled = false;
-this.__defineGetter__("pluginEnabled", function() {
-	return this._pluginEnabled;
+var _addon, _addonId;
+this._pluginDisabled = undefined;
+this.__defineGetter__("pluginDisabled", function() {
+	return this._pluginDisabled;
 });
-this.__defineSetter__("pluginEnabled", function(enabled) {
-	if(this._pluginEnabled == enabled)
+this.__defineSetter__("pluginDisabled", function(dis) {
+	if(this._pluginDisabled == dis)
 		return;
-	this._pluginEnabled = enabled;
-	if(options.checkedStyle)
-		this.checked = enabled;
-	if(options.disabledGrayscale)
-		this.style.filter = enabled ? "" : 'url("chrome://mozapps/skin/extensions/extensions.svg#greyscale")';
-	if(options.disabledOpacity) {
+	this._pluginDisabled = dis;
+
+	var style;
+	if("STATE_ASK_TO_ACTIVATE" in AddonManager && dis == AddonManager.STATE_ASK_TO_ACTIVATE)
+		style = options.styleClickToPlay;
+	else if(!dis)
+		style = options.styleEnabled;
+	else
+		style = options.styleDisabled;
+
+	if(style.hasOwnProperty("checked"))
+		this.checked = style.checked;
+	if(style.hasOwnProperty("style"))
+		this.style.cssText = style.style;
+	if(
+		style.hasOwnProperty("iconStyle")
+		|| style.hasOwnProperty("iconGrayscale")
+		|| style.hasOwnProperty("iconOpacity")
+	) {
 		var icon = this.ownerDocument.getAnonymousElementByAttribute(this, "class", "toolbarbutton-icon");
-		if(icon)
-			icon.style.opacity = enabled ? "" : options.disabledOpacity;
+		if(icon) {
+			if(style.hasOwnProperty("iconStyle"))
+				icon.style.cssText = style.iconStyle;
+			if(style.hasOwnProperty("iconGrayscale"))
+				icon.style.filter = style.iconGrayscale ? 'url("chrome://mozapps/skin/extensions/extensions.svg#greyscale")' : "";
+			if(style.hasOwnProperty("iconOpacity"))
+				icon.style.opacity = style.iconOpacity;
+		}
 	}
 });
 Components.utils.import("resource://gre/modules/AddonManager.jsm");
@@ -40,13 +83,15 @@ this.initAddonListener = function() {
 	var addonListener = {
 		button: this,
 		onEnabled: function(addon) {
-			if(addon.id == addonId)
-				this.button.pluginEnabled = true;
+			this._updateButton(addon);
 		},
 		onDisabled: function(addon) {
-			if(addon.id == addonId)
-				this.button.pluginEnabled = false;
-		}
+			this._updateButton(addon);
+		},
+		_updateButton: function(addon) {
+			if(addon.id == _addonId)
+				this.button.pluginDisabled = addon.userDisabled;
+		},
 	};
 	AddonManager.addAddonListener(addonListener);
 	this.onDestroy = function() {
@@ -59,24 +104,43 @@ AddonManager.getAddonsByTypes(options.searchInTypes, function(addons) {
 	addons.some(function(addon) {
 		if(addon.name.indexOf(options.pluginName) == -1)
 			return false;
-		addonId = addon.id;
-		btn.pluginEnabled = !addon.userDisabled;
+		_addonId = addon.id;
+		_addon = addon;
+		btn.pluginDisabled = addon.userDisabled;
 		btn.initAddonListener();
 		return true;
 	});
-	if(!addonId)
-		btn.pluginEnabled = false;
+	if(!_addon)
+		btn.pluginDisabled = true;
 });
 
 this.onclick = function(e) {
 	if(e.button != 0)
 		return;
-	if(!addonId) {
+	if(!_addon) {
 		alert(options.pluginName + " not installed!");
 		return;
 	}
-	this.pluginEnabled = !this._pluginEnabled; // Faster than callback
-	AddonManager.getAddonByID(addonId, function(addon) {
-		addon.userDisabled = !addon.userDisabled;
-	});
+	// Note: we manually updates styles because this is a bit faster, than callback in case of addon changes
+	_addon.userDisabled = this.pluginDisabled = getNewDisabled(_addon);
 };
+function getNewDisabled(addon) {
+	// disabled -> STATE_ASK_TO_ACTIVATE -> enabled -> ...
+	var curDis = addon.userDisabled;
+	if("STATE_ASK_TO_ACTIVATE" in AddonManager && curDis == AddonManager.STATE_ASK_TO_ACTIVATE)
+		newDis = false;
+	else if(!curDis)
+		newDis = true;
+	else {
+		if(isAskToActivateAddon(addon))
+			newDis = AddonManager.STATE_ASK_TO_ACTIVATE;
+		else
+			newDis = false;
+	}
+	return newDis;
+}
+function isAskToActivateAddon(addon) {
+	return addon.type == "plugin"
+		&& "STATE_ASK_TO_ACTIVATE" in AddonManager
+		&& Application.prefs.getValue("plugins.click_to_play", false);
+}
