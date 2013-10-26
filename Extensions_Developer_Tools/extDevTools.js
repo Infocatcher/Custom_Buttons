c331a9c0f16f5b2ba6536572bf373a883f5f98db
Infocatcher <infocatcher.dev.null@gmail.com>
2013-10-26 20:05:40 +0400

    Bump version to 0.1.2pre4 - 2013-10-26

--- Extensions_Developer_Tools/extDevTools.js
+++ Extensions_Developer_Tools/extDevTools.js
@@ -6,7 +6,7 @@
 // (code for "initialization" section)
 
 // (c) Infocatcher 2011-2013
-// version 0.1.2pre3 - 2013-09-09
+// version 0.1.2pre4 - 2013-10-26
 
 // Includes Attributes Inspector
 //   http://infocatcher.ucoz.net/js/cb/attrsInspector.js
