6547088c51f038cfd7989bb63a29cb6e63066194
Infocatcher <infocatcher.dev.null@gmail.com>
2013-11-04 22:31:49 +0400

    Simplify nsITimer usage: use simple callback function instead of nsIObserver implementation

--- Attributes_Inspector/attrsInspector.js
+++ Attributes_Inspector/attrsInspector.js
@@ -733,11 +733,9 @@ function init() {
 			var _timers = this._timers;
 			var timer = _timers[id] = Components.classes["@mozilla.org/timer;1"]
 				.createInstance(Components.interfaces.nsITimer);
-			timer.init({
-				observe: function(subject, topic, data) {
-					delete _timers[id];
-					callback.apply(context, args);
-				}
+			timer.init(function() {
+				delete _timers[id];
+				callback.apply(context, args);
 			}, delay || 0, timer.TYPE_ONE_SHOT);
 			return id;
 		},
