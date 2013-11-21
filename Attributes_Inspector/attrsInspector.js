Infocatcher/Custom_Buttons

commit b22c2e790500b4359d3d70e1e27c3fc870d39e2a

Simplify inspect() function

Attributes_Inspector/attrsInspector.js
@@ -1767,11 +1767,7 @@ function init() {
 				);
 				var tryDelay = 5;
 				function inspect() {
-					if(!inspWin.inspector) {
-						inspWin.setTimeout(inspect, tryDelay);
-						return;
-					}
-					try {
+					if("inspector" in inspWin) try {
 						try {
 							// Avoid warnings in error console after getViewer("dom")
 							var hash = inspWin.inspector.mPanelSet.registry.mViewerHash;

commit cd82cc165cbe6a55a7b1375805b5932062f22307

Fix regression in inspect() function window.inspector may be undefined (introduced in b22c2e790500b4359d3d70e1e27c3fc870d39e2a commit)

Attributes_Inspector/attrsInspector.js
@@ -1768,7 +1768,7 @@ function init() {
 				);
 				var tryDelay = 5;
 				function inspect() {
-					if("inspector" in inspWin) try {
+					if("inspector" in inspWin && inspWin.inspector) try {
 						try {
 							// Avoid warnings in error console after getViewer("dom")
 							var hash = inspWin.inspector.mPanelSet.registry.mViewerHash;