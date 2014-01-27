Infocatcher/Custom_Buttons

commit 959857fced0a22318a2eedce026976836c799c46

Use nsITimer instead of setTimeout() for delayed savePrefFile(): setTimeout() doesn't work with "Switch locale" command

Extensions_Developer_Tools/extDevTools.js
@@ -943,10 +943,13 @@ var cmds = this.commands = {
 		if(!force && !this.prefsChanged)
 			return;
 		this.prefsChanged = false;
-		setTimeout(function(_this) {
+		var _this = this;
+		var timer = Components.classes["@mozilla.org/timer;1"]
+			.createInstance(Components.interfaces.nsITimer);
+		timer.init(function() {
 			_this.prefSvc.savePrefFile(null);
 			LOG("savePrefFile()");
-		}, 100, this);
+		}, 100, timer.TYPE_ONE_SHOT);
 	},
 
 	handleEvent: function(e) {

commit 3d380e917964b39eb73f27e7c199ec8c0603c50c

Correctly save prefs file in Firefox 3.6 and older (regression in 959857fced0a22318a2eedce026976836c799c46 commit)

Extensions_Developer_Tools/extDevTools.js
@@ -1388,9 +1388,11 @@ var cmds = this.commands = {
 		var _this = this;
 		var timer = Components.classes["@mozilla.org/timer;1"]
 			.createInstance(Components.interfaces.nsITimer);
-		timer.init(function() {
-			_this.prefSvc.savePrefFile(null);
-			LOG("savePrefFile()");
+		timer.init({
+			observe: function() {
+				_this.prefSvc.savePrefFile(null);
+				LOG("savePrefFile()");
+			}
 		}, 100, timer.TYPE_ONE_SHOT);
 	},
 