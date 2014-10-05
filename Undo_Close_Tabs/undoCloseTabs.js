Infocatcher/Custom_Buttons

commit cf04a4716c2a7c0b41904d412bf126305f1e6d84

Fix "useMenu: true": move tooltip into #mainPopupSet

Undo_Close_Tabs/undoCloseTabs.js
@@ -435,7 +435,7 @@ this.undoCloseTabsList = {
 		btn.removeAttribute("tooltiptext");
 		btn.setAttribute("tooltip", this.tipId);
 		btn.setAttribute("popupsinherittooltip", "true");
-		btn.appendChild(tip);
+		document.getElementById("mainPopupSet").appendChild(tip);
 	},
 	_hasPbExitObserver: false,
 	addPbExitObserver: function(add) {

commit 70cee459bb251fea10a8c9f2e463c7346bf0559d

Fix tooltip (regression in cf04a4716c2a7c0b41904d412bf126305f1e6d84 commit)

Undo_Close_Tabs/undoCloseTabs.js
@@ -413,10 +413,11 @@ this.undoCloseTabsList = {
 		tip = this.tip = this.createElement("tooltip", {
 			id: this.tipId,
 			orient: "vertical",
-			onpopupshowing: "return this.parentNode.undoCloseTabsList.updTooltip(this, document.tooltipNode);",
+			onpopupshowing: "return this.undoCloseTabsList.updTooltip(this, document.tooltipNode);",
 			onpopuphiding: "this.cancelUpdateTimer();",
 			style: "padding: 0;"
 		});
+		tip.undoCloseTabsList = this;
 		tip._updateTimer = 0;
 		tip.initUpdateTimer = function(fn, context) {
 			if(this._updateTimer)