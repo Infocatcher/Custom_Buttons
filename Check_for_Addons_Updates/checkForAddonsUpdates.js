f3ace8ffefadffb5247f9fe12aee6b4870d58bb2
Infocatcher <infocatcher.dev.null@gmail.com>
2014-10-13 20:14:04 +0400

    Bump version to 0.1.5 - 2014-10-13

--- Check_for_Addons_Updates/checkForAddonsUpdates.js
+++ Check_for_Addons_Updates/checkForAddonsUpdates.js
@@ -6,7 +6,7 @@
 // (code for "code" section)
 
 // (c) Infocatcher 2012-2014
-// version 0.1.5pre2 - 2014-07-23
+// version 0.1.5 - 2014-10-13
 
 // Button just open hidden tab with about:addons and trigger built-in "Check for Updates" function.
 // And show tab, if found updates.
