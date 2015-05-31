82c58b1a338caa2071426cb18d0e7e951179954a
Infocatcher <infocatcher.dev.null@gmail.com>
2015-05-31 15:36:50 +0300

    Bump version to 0.1.4.2 - 2015-05-31

--- Toggle_Flash/toggleFlash.js
+++ Toggle_Flash/toggleFlash.js
@@ -4,8 +4,8 @@
 // Toggle Flash button for Custom Buttons
 // (code for "initialization" section)
 
-// (c) Infocatcher 2012-2014
-// version 0.1.4.1 - 2014-02-21
+// (c) Infocatcher 2012-2015
+// version 0.1.4.2 - 2015-05-31
 
 var options = {
 	pluginName: "Shockwave Flash", // Or name of any other plugin
