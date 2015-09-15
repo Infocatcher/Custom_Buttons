Infocatcher/Custom_Buttons

commit ec28ad7a0bf9f6077551338df10fcd6c894db299

Simplify: use try/catch to safely get nsIURI.host

Cookies_Permissions/cookiesPermissions.js
@@ -579,18 +579,11 @@ this.permissions = {
 	},
 	getHostFromBrowser: function(browser) {
 		try {
-			let uri = browser.currentURI;
-			if(!this.noHost(uri.scheme))
-				return uri.host;
+			return browser.currentURI.host;
 		}
 		catch(e) {
-			Components.utils.reportError(e);
 		}
 		return "";
-	},
-	noHost: function(protocol) {
-		protocol = String.replace(protocol, /:$/, "");
-		return ["view-source", "about", "chrome", "resource", "javascript", "data"].indexOf(protocol) != -1;
 	},
 	get currentProtocol() {
 		var scheme = gBrowser.currentURI.scheme;

commit 9fd6700c8d6cb6d2d200d00cd28b8888168c0382

Don't use host from chrome:// and resource:// URIs (regression in ec28ad7a0bf9f6077551338df10fcd6c894db299 commit)

Cookies_Permissions/cookiesPermissions.js
@@ -579,7 +579,10 @@ this.permissions = {
 	},
 	getHostFromBrowser: function(browser) {
 		try {
-			return browser.currentURI.host;
+			var uri = browser.currentURI;
+			if(["chrome", "resource"].indexOf(uri.scheme) != -1)
+				return "";
+			return uri.host;
 		}
 		catch(e) {
 		}

Plugins_Permissions/pluginsPermissions.js
@@ -495,7 +495,10 @@ this.permissions = {
 	},
 	getHostFromBrowser: function(browser) {
 		try {
-			return browser.currentURI.host;
+			var uri = browser.currentURI;
+			if(["chrome", "resource"].indexOf(uri.scheme) != -1)
+				return "";
+			return uri.host;
 		}
 		catch(e) {
 		}