c47c1ea98c5c12962f6d14cba4fa702a9161c1d3
Infocatcher <infocatcher.dev.null@gmail.com>
2017-03-10 00:00:30 +0300

    Update copyright + bump version to 0.1.0b1pre3 - 2016-03-23

--- Convert_E4X/convertE4X.js
+++ Convert_E4X/convertE4X.js
@@ -5,8 +5,8 @@
 // Convert E4X button for Custom Buttons
 // Code for "code" section
 
-// (c) Infocatcher 2012-2013
-// version 0.1.0b1pre2 - 2013-02-17
+// (c) Infocatcher 2012-2013, 2016
+// version 0.1.0b1pre3 - 2016-03-23
 
 // Tries convert deprecated E4X to string literals.
 // Click on button and then click on another button with E4X in code (or click on opened page with *.js file).
