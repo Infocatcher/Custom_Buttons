12b127a7fdf56c1f0820da9ddc24479297471e15
Infocatcher <infocatcher.dev.null@gmail.com>
2020-04-25 00:00:37 +0300

    Rewrite: correctly call in window environment

--- code_snippets/customizableUI.js
+++ code_snippets/customizableUI.js
@@ -5,26 +5,16 @@
 // See "//!" comments
 
 (function () {
-// Custom Buttons-like environment
-var event = {};
-var _id, _phase = "code";
-var xulns = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
-var xhtmlns = "http://www.w3.org/1999/xhtml";
-function LOG(msg) {
-	var head = "[Custom Buttons CustomizableUI.jsm: id: " + _id + "@" + _phase
-		+ ", line: " + Components.stack.caller.lineNumber + "]";
-	Services.console.logStringMessage(head + "\n" + msg);
-}
-
 CustomizableUI.createWidget({
 	id: "__cb_someCustomButton", //! Change to something unique
 	type: "custom",
 	// https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/CustomizableUI.jsm#Area_constants
 	defaultArea: CustomizableUI.AREA_NAVBAR, //! Default toolbar
 	onBuild: function(doc) {
-		var btn = doc.createElementNS(xulns, "toolbarbutton");
+		var btn = doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "toolbarbutton");
+		var id = this.id;
 		var attrs = {
-			id: (_id = this.id),
+			id: id,
 			class: "toolbarbutton-1 chromeclass-toolbar-additional",
 			label: "Button name", //! Set label here
 			tooltiptext: "Button tooltip", //! Set tooltip here
@@ -33,25 +23,52 @@ CustomizableUI.createWidget({
 		};
 		for(var p in attrs)
 			btn.setAttribute(p, attrs[p]);
-		btn.addEventListener("command", cbCode.bind(btn), false);
-		doc.defaultView.setTimeout(function() {
-			try {
-				_phase = "init";
-				cbInitialization.call(btn);
-			}
-			finally {
-				_phase = "code";
-			}
+		btn.addEventListener("command", function(e) {
+			new win.Function("e",
+				"("
+				+ cbCode.toString()
+					.replace("{", "{" + cbEnv(id))
+				+ ").call(document.getElementById('" + id + "'), e);"
+			)(e);
+		}, false);
+		var win = doc.defaultView;
+		win.setTimeout(function() {
+			new win.Function(
+				"("
+				+ cbInitialization.toString()
+					.replace("{", "{" + cbEnv(id).replace("@code", "@init"))
+				+ ").call(document.getElementById('" + id + "'));"
+			)();
 		}, 0);
 		return btn;
 	}
 });
 
+function cbEnv(id) {
+	var envCode = ("" + function() {
+		// Custom Buttons-like environment
+		var event = {};
+		var xulns = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+		var xhtmlns = "http://www.w3.org/1999/xhtml";
+		function LOG(msg) {
+			var head = "[Custom Buttons CustomizableUI.jsm: id: #id@code"
+				+ ", line: " + Components.stack.caller.lineNumber + "]";
+			Services.console.logStringMessage(head + "\n" + msg);
+		}
+	}).replace("#id", id).replace(/^[\s\S]+?\{|\}[^}]*?$/g, "");
+	Components.classes["@mozilla.org/consoleservice;1"]
+		.getService(Components.interfaces.nsIConsoleService)
+		.logStringMessage("xxx\n\n" + envCode);
+	return envCode;
+}
+
 function cbCode() {
 	//! Place code here
+	LOG("cbCode()");
 }
 function cbInitialization() {
 	//! Place initialization here
+	LOG("cbInitialization()");
 }
 
 })();
