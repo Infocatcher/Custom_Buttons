00485bae186fdebb776769148aac235cd655f72f
Infocatcher <infocatcher.dev.null@gmail.com>
2020-05-17 00:02:42 +0300

    Optimize: simplify for multiple buttons

--- code_snippets/customizableUI.js
+++ code_snippets/customizableUI.js
@@ -2,10 +2,8 @@
 // Dummy wrapper to create Custom Buttons using CustomizableUI.jsm
 // https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/CustomizableUI.jsm
 
-(function () {
-
 //== Configuration begin
-var cb = {
+CustomButton({
 	id: "someCustomButton", // Unique identifier
 	fileInit: "someCustomButton.js", // Relative path to script file or empty string ("")
 	fileCode: "", // Relative path to script file or empty string ("")
@@ -14,61 +12,63 @@ var cb = {
 	icon: "chrome://branding/content/icon16.png",
 	// https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/CustomizableUI.jsm#Area_constants
 	defaultArea: CustomizableUI.AREA_NAVBAR // Default toolbar
-};
+});
 //== Configuration end
 
-var cbEnv = {
-	xulns: "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
-	xhtmlns: "http://www.w3.org/1999/xhtml",
-	LOG: function(msg) {
-		var head = "[Custom Buttons CustomizableUI.jsm: id: " + this._id + "@" + this._phase
-			+ ", line: " + Components.stack.caller.lineNumber + "]";
-		Services.console.logStringMessage(head + "\n" + msg);
-	}
-};
+function CustomButton(cb) {
+	var cbEnv = {
+		xulns: "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+		xhtmlns: "http://www.w3.org/1999/xhtml",
+		LOG: function(msg) {
+			var head = "[Custom Buttons CustomizableUI.jsm: id: " + this._id + "@" + this._phase
+				+ ", line: " + Components.stack.caller.lineNumber + "]";
+			Services.console.logStringMessage(head + "\n" + msg);
+		}
+	};
 
-var path = new Error().fileName.replace(/[^\\\/]*$/, "");
-function cbExec(win, btn, codeEvent) {
-	// Note: be careful with global variables inside cb-script
-	var context = Object.assign(btn, cbEnv, {
-		_id: btn.id,
-		_phase: codeEvent ? "code" : "init",
-		event: codeEvent || new win.Object(),
-		self: btn
-	});
-	context.LOG = context.LOG.bind(context);
-	Object.defineProperty(context, "top", { value: win.top });
-	var file = path + (codeEvent ? cb.fileCode : cb.fileInit);
-	context.LOG("cbExec()\n" + file);
-	Services.scriptloader.loadSubScript(file, context, "UTF-8");
+	var path = new Error().fileName.replace(/[^\\\/]*$/, "");
+	function cbExec(cb, win, btn, codeEvent) {
+		// Note: be careful with global variables inside cb-script
+		var context = Object.assign(btn, cbEnv, {
+			_id: btn.id,
+			_phase: codeEvent ? "code" : "init",
+			event: codeEvent || new win.Object(),
+			self: btn
+		});
+		context.LOG = context.LOG.bind(context);
+		Object.defineProperty(context, "top", { value: win.top });
+		var file = path + (codeEvent ? cb.fileCode : cb.fileInit);
+		context.LOG("cbExec()\n" + file);
+		Services.scriptloader.loadSubScript(file, context, "UTF-8");
+	}
+
+	(CustomButton = function(cb) {
+		CustomizableUI.createWidget({
+			id: "__cb_" + cb.id,
+			type: "custom",
+			defaultArea: cb.defaultArea,
+			onBuild: function(doc) {
+				var btn = doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "toolbarbutton");
+				var id = cbEnv._id = this.id;
+				var attrs = {
+					id: id,
+					class: "toolbarbutton-1 chromeclass-toolbar-additional",
+					label: cb.label,
+					tooltiptext: cb.tooltip,
+					style: 'list-style-image: url("' + cb.icon + '");',
+					__proto__: null
+				};
+				for(var p in attrs)
+					btn.setAttribute(p, attrs[p]);
+				var win = doc.defaultView;
+				cb.fileCode && btn.addEventListener("command", function(e) {
+					cbExec(cb, win, btn, e);
+				}, false);
+				cb.fileInit && win.setTimeout(function() {
+					cbExec(cb, win, btn, false);
+				}, 0);
+				return btn;
+			}
+		});
+	})(cb);
 }
-
-CustomizableUI.createWidget({
-	id: "__cb_" + cb.id,
-	type: "custom",
-	defaultArea: cb.defaultArea,
-	onBuild: function(doc) {
-		var btn = doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "toolbarbutton");
-		var id = cbEnv._id = this.id;
-		var attrs = {
-			id: id,
-			class: "toolbarbutton-1 chromeclass-toolbar-additional",
-			label: cb.label,
-			tooltiptext: cb.tooltip,
-			style: 'list-style-image: url("' + cb.icon + '");',
-			__proto__: null
-		};
-		for(var p in attrs)
-			btn.setAttribute(p, attrs[p]);
-		var win = doc.defaultView;
-		cb.fileCode && btn.addEventListener("command", function(e) {
-			cbExec(win, btn, e);
-		}, false);
-		cb.fileInit && win.setTimeout(function() {
-			cbExec(win, btn, false);
-		}, 0);
-		return btn;
-	}
-});
-
-})();
